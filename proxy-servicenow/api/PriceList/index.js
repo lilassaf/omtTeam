2659fc2c874e90af15b344fc431b05fef423b74c
MCDIYAE <meddiyae.ghbalou@gmail.com>
2025-07-04 19:05:53 +0100

    merge main

--- proxy-servicenow/api/PriceList/index.js
+++ proxy-servicenow/api/PriceList/index.js
@@ -14,10 +14,6 @@ router.get('/price-list', getAll);
 router.get('/price-list/:id', getOne);
 router.post('/price-list', create);
 router.delete('/price-list/:id', deletePrice);
-<<<<<<< HEAD
-router.get('/price-list/:id',getOne);
-=======
 router.get('/price-list/:id', getOne);
->>>>>>> f51a9c582772a7a89a4b8d2dd5ecee26195e2add
 
 module.exports = router;
--- proxy-servicenow/controllers/PriceList/getOnePriceList.js
+++ proxy-servicenow/controllers/PriceList/getOnePriceList.js
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-const priceList = require("../../models/priceList");
-const { isValidObjectId } = require('mongoose');
-const handleMongoError = require('../../utils/handleMongoError');
-
-module.exports = async (req, res) => {
-    try {
-        const { id } = req.params;
-        // Validate MongoDB ID format
-        if (!isValidObjectId(id)) {
-            return res.status(400).json({
-                success: false,
-                error: 'Invalid ID format',
-                message: 'Please provide a valid MongoDB ObjectID'
-            });
-        }
-
-        const data = await priceList.findById(id);
-
-        if (!data) return res.status(404).send({ message: 'Price List not found' });
-        res.send(data);
-    } catch (error) {
-         // Handle MongoDB-specific errors
-    if (error.name === 'CastError' || error.name.includes('Mongo')) {
-        const mongoError = handleMongoError(error);
-        return res.status(mongoError.status).json({ 
-          success: false,
-          error: mongoError.message,
-          details: mongoError.details 
-        });
-      }
-      
-      // Handle other unexpected errors
-      return res.status(500).json({ 
-        success: false,
-        error: 'Internal Server Error',
-        message: error.message
-      });
-    }
-=======
 
 const PriceList = require('../../models/priceList');
 const getProductOfferingPriceByPriceList = require('../ProductOfferingPrice/getProductOfferingPriceByPriceList_includePO');
@@ -55,5 +15,4 @@ module.exports = async (req, res) => {
     const mongoError = handleMongoError(error);
     return res.status(mongoError.status).json({ error: mongoError.message });
   }
->>>>>>> f51a9c582772a7a89a4b8d2dd5ecee26195e2add
 };
