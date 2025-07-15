81452413cca651b2a9c0655200ca36d55d2963db
Yassir Haiddouch <haiddouchy5@gmail.com>
2025-07-15 12:09:57 +0100

    fix api axios

--- react-servicenow/src/utils/axioswithsession.js
+++ react-servicenow/src/utils/axioswithsession.js
@@ -2,14 +2,17 @@ import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_BACKEND_URL;
 
-const getHeaders = () => ({
-  'Content-Type': 'application/json',
-  'authorization': `${localStorage.getItem('access_token')}`,
-});
+const getHeaders = async () => {  
+  return {
+    'Content-Type': 'application/json',
+    'authorization': `${localStorage.getItem('access_token')}`,
+  };
+};
+ const headers = await getHeaders();
 
 export const api = axios.create({
   baseURL: API_URL,
   timeout: 10000,
-  headers: getHeaders(),
+  headers: headers,
 });
 
